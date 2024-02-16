026cb5ba13173832be75ad3b5ef1440b2c0033d2
Utsav-nith <utsavchy.dav47@gmail.com>
2024-02-16 11:17:01 +0530

    crud added

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -18,9 +18,28 @@ class PostsController < ApplicationController
         @post = Post.find(params[:id])
     end
 
+    def update 
+        @post =Post.find(params[:id])
+        if @post.update(post_params)
+            redirect_to posts_path
+        else
+            render 'edit'
+        end
+    end
+
     def edit 
         @post =Post.find(params[:id])
     end
+
+    def destroy
+        #puts "deleted----------------------------------"
+        @post = Post.find(params[:id])
+        @post.destroy
+      
+        redirect_to posts_path
+    end
+      
+
     private
     def post_params
         # params is a hash-like object that contains all the parameters sent with the HTTP request.
